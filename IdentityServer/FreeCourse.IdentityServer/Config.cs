d1d3ba4c3b91c2efb36e90a913d271fb200ef85e
hsnoguz <hsnoguz@yahoo.com>
2024-02-11 23:37:50 +0300

    Web

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -17,7 +17,10 @@ namespace FreeCourse.IdentityServer
      new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
                new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission"}},
                new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
-               
+               new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
+               new ApiResource("resource_order"){Scopes={"order_fullpermission"}},
+               new ApiResource("resource_payment"){Scopes={"payment_fullpermission"}},
+               new ApiResource("resource_gateway"){Scopes={"gateway_fullpermission"}},
                new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -37,6 +40,10 @@ namespace FreeCourse.IdentityServer
 
                 new ApiScope("photo_stock_fullpermission","Photo Stock API için full erişim"),
                 new ApiScope("basket_fullpermission","Basket API için full erişim"),
+                new ApiScope("discount_fullpermission","Discount API için full erişim"),
+                new ApiScope("order_fullpermission","Order API için full erişim"),
+                new ApiScope("payment_fullpermission","Payment API için full erişim"),
+                new ApiScope("gateway_fullpermission","Gateway API için full erişim"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName),
             };
 
@@ -47,10 +54,10 @@ namespace FreeCourse.IdentityServer
                 {
                     ClientName = "Asp.Net Core MVC",
                     ClientId = "WebMvcClient",
-                ClientSecrets= {new Secret("secret".Sha256())},
+                    ClientSecrets= {new Secret("secret".Sha256())},
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-           AllowedScopes={ "catalog_fullpermission", "photo_stock_fullpermission", "gateway_fullpermission", IdentityServerConstants.LocalApi.ScopeName }
-                },
+                    AllowedScopes={ "catalog_fullpermission", "photo_stock_fullpermission", "gateway_fullpermission", IdentityServerConstants.LocalApi.ScopeName }
+                }, 
                 
                 new Client
                 {
@@ -62,6 +69,10 @@ namespace FreeCourse.IdentityServer
                        
                     AllowedScopes = {
                         "basket_fullpermission",
+                        "discount_fullpermission",
+                        "order_fullpermission",
+                        "payment_fullpermission",
+                        "gateway_fullpermission",
                         IdentityServerConstants.StandardScopes.Email,
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile, 
---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
@@ -0,0 +1,46 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class Order:Entity, IAggregateRoot
+    {
+        public DateTime CreatedDate { get; private set; }
+
+        public Address Address { get; private set; } 
+        public string BuyerId { get; private set; }
+
+        private readonly List<OrderItem> _orderItem;
+
+         public IReadOnlyCollection<OrderItem> OrderItems  => _orderItem;
+        public Order()
+        {
+
+        }
+        public Order(string buyerId, Address address)
+        {
+            Address = address;
+            BuyerId = buyerId;
+            CreatedDate = DateTime.Now;
+            _orderItem = new List<OrderItem>();
+        }
+
+        public void AddOrderItem(string productId, string productName, decimal price, string pictureUrl)
+        {
+            var existProduct = _orderItem.Any(x => x.ProductId == productId);
+            if(!existProduct)
+            {
+                var newOrderItem = new OrderItem(productId, productName, pictureUrl, price);
+                _orderItem.Add(newOrderItem);
+            }
+        }
+
+        public decimal TotalPrice => _orderItem.Sum(x => x.Price);
+
+    }
+}
